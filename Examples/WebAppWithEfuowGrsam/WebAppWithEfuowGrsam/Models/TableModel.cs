0acbee04f9b8f3c2095739b2377b066f843fbf24
tgrymnak <taras.grymnak@gmail.com>
2016-01-22 17:08:05 +0100

    Add Examples folder

---
+++ Examples/WebAppWithEfuowGrsam/WebAppWithEfuowGrsam/Models/TableModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebAppWithEfuowGrsam.Models
+{
+    public enum TableStatus
+    {
+        None = 0,
+        Added = 1,
+        Removed = 2
+    }
+
+    public class TableModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public TableStatus Status { get; set; }
+    }
+}

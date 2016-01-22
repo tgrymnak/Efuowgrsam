0ee3f57ce63f54e6488ea5401fa0c8913de1e5bb
tgrymnak <taras.grymnak@gmail.com>
2016-01-22 16:38:36 +0100

    Added example

---
+++ WebAppWithEfuowGrsam/WebAppWithEfuowGrsam/Services/TableService.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using EfuowGrsam.Services;
+using WebAppWithEfuowGrsam.DAL;
+using WebAppWithEfuowGrsam.Models;
+
+namespace WebAppWithEfuowGrsam.Services
+{
+    public class TableService: GenericService<Table, TableModel>
+    {
+        public TableService() : base(new TestDbEntities()) { }
+
+        protected override void ConfigureAutoMappings()
+        {
+            Mapper.CreateMap<Table, TableModel>()
+                .ForMember(dest => dest.Status, opt => opt.MapFrom(src => (TableStatus)src.Status));
+            Mapper.CreateMap<TableModel, Table>()
+                .ForMember(dest => dest.Status, opt => opt.MapFrom(src => (int)src.Status));
+        }
+    }
+}

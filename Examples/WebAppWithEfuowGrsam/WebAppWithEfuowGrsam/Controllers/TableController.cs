88d393ffb83adaf96999200d362881a82f4eafc9
tgrymnak <taras.grymnak@gmail.com>
2016-01-22 17:00:33 +0100

    Add Examples folder

---
+++ Examples/WebAppWithEfuowGrsam/WebAppWithEfuowGrsam/Controllers/TableController.cs
@@ -0,0 +1,35 @@
+﻿using System.Web.Mvc;
+using WebAppWithEfuowGrsam.Models;
+using WebAppWithEfuowGrsam.Services;
+
+namespace WebAppWithEfuowGrsam.Controllers
+{
+    public class TableController : Controller
+    {
+        private TableService service = new TableService();
+
+        // GET: Table
+        public ActionResult Index()
+        {
+            var model = service.GetAll();
+            return View(model);
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(TableModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                service.Create(model);
+                return RedirectToAction("Index");
+            }
+
+            return View(model);
+        }
+    }
+}
